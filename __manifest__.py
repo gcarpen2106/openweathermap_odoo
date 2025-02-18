195dd461c29d424e700ada3ce7bb8f682f1ea49b
gcarpen2106 <gcarpen2106@gmail.com>
2025-02-18 08:28:57 +0100

    Editado archivo __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+ 'name': 'OpenWeatherMap Integration',
+ 'version': '1.0',
+ 'summary': 'Integración con la API de OpenWeatherMap',
+ 'description': 'Obtención de datos meteorológicos en tiempo real.',
+ 'author': 'Paco Navas',
+ 'category': 'Tools',
+ 'depends': ['base'],
+ 'data': [
+ 'security/ir.model.access.csv',
+ 'views/weather_view.xml',
+ ],
+ 'installable': True,
+ 'application': True,
+ 'license': 'LGPL-3',
+ 'icon': ['/openweathermap_odoo/ static/description/icon57.png'],
+}
